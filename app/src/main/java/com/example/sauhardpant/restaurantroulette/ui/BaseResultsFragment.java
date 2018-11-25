07c3b32f39eb71543864ed4ecf634e062c912e7a
tjarpsp <tjarpsp@gmail.com>
2018-11-25 13:55:22 -0500

    Callback interface to return data to fragment

--- app/src/main/java/com/example/sauhardpant/restaurantroulette/ui/BaseResultsFragment.java
+++ app/src/main/java/com/example/sauhardpant/restaurantroulette/ui/BaseResultsFragment.java
@@ -2,7 +2,9 @@ package com.example.sauhardpant.restaurantroulette.ui;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -11,25 +13,43 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
+import com.apollographql.apollo.yelp.SearchYelpQuery;
 import com.example.sauhardpant.restaurantroulette.R;
 import com.example.sauhardpant.restaurantroulette.ViewModel.BaseResultsViewModel;
 import com.tbruyelle.rxpermissions2.RxPermissions;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class BaseResultsFragment extends Fragment {
     private static final String TAG = BaseResultsFragment.class.getSimpleName();
     private BaseResultsViewModel viewModel;
 
+    @BindView(R.id.lvBusinesses)
+    ListView lvBusinesses;
+
     public BaseResultsFragment() {}
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        viewModel = ViewModelProviders.of(this).get(BaseResultsViewModel.class);
+        viewModel.init(getContext());
+
+        subscribeToViewModels();
+    }
 
     @SuppressLint("CheckResult")
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_base_results, container, false);
-
-        viewModel = ViewModelProviders.of(this).get(BaseResultsViewModel.class);
-        viewModel.init(getContext());
+        ButterKnife.bind(this, rootView);
 
         final RxPermissions rxPermissions = new RxPermissions(this);
         rxPermissions.request(Manifest.permission.ACCESS_FINE_LOCATION)
@@ -42,5 +62,23 @@ public class BaseResultsFragment extends Fragment {
                 });
 
         return rootView;
+    }
+
+    public void subscribeToViewModels() {
+        viewModel.getLocation().observe(this, new Observer<Location>() {
+            @Override
+            public void onChanged(@Nullable Location location) {
+                Log.d(TAG, "on location changed: " + location.getLongitude() + ", " + location.getLatitude());
+            }
+        });
+        
+        viewModel.getBusinessList().observe(this, new Observer<List<SearchYelpQuery.Business>>() {
+            @Override
+            public void onChanged(@Nullable List<SearchYelpQuery.Business> businesses) {
+                for (int i = 0; i < businesses.size(); i++) {
+                    Log.d(TAG, "onChanged: " + businesses.get(i).name() + "\n");
+                }
+            }
+        });
     }
 }
